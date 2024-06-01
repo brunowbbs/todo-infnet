aea4a0e9781cf8078742f48db4ef5e4705d44ea4
Wesley Bruno <engwesleybruno@gmail.com>
2024-05-31 21:57:51 -0300

    feat: editing fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { useState, useEffect } from "react";
 import Card from "./components/card";
+
+import Modal from "react-modal";
+
 import "./styles.css";
 
-//local storage
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
-// PROPS, STATES, SIDE EFFECTS
+Modal.setAppElement("#root");
 
 export default function App() {
   const [titulo, setTitulo] = useState("");
@@ -12,7 +24,13 @@ export default function App() {
   const [data, setData] = useState("");
   const [descricao, setDescricao] = useState("");
 
+  const [indexSelecionado, setIndexSelecionado] = useState(null);
+
+  const [indexParaRemover, setIndexParaRemover] = useState(null);
+
   const [tarefas, setTarefas] = useState([]);
+
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     getTarefasLocalStorage();
@@ -49,11 +67,71 @@ export default function App() {
     setDescricao("");
   }
 
+  function editarTarefa(event) {
+    event.preventDefault();
+
+    // const novaTarefa = {
+    //   titulo: titulo,
+    //   categoria: categoria,
+    //   data: data,
+    //   descricao: descricao,
+    // };
+
+    const tempTarefas = [...tarefas];
+
+    tempTarefas[indexSelecionado].titulo = titulo;
+    tempTarefas[indexSelecionado].categoria = categoria;
+    tempTarefas[indexSelecionado].data = data;
+    tempTarefas[indexSelecionado].descricao = descricao;
+
+    setTarefas(tempTarefas);
+
+    const tarefasString = JSON.stringify([...tempTarefas]);
+
+    localStorage.setItem("@tarefas", tarefasString);
+
+    setTitulo("");
+    setCategoria("");
+    setData("");
+    setDescricao("");
+    setIndexSelecionado(null);
+  }
+
+  function preencheForm(tarefa, index) {
+    setTitulo(tarefa.titulo);
+    setCategoria(tarefa.categoria);
+    setData(tarefa.data);
+    setDescricao(tarefa.descricao);
+    setIndexSelecionado(index);
+  }
+
+  function removerTarefa() {
+    const tarefasFiltradas = tarefas.filter(
+      (tarefa, indexTarefa) => indexTarefa !== indexParaRemover
+    );
+
+    localStorage.setItem("@tarefas", JSON.stringify(tarefasFiltradas));
+
+    setTarefas(tarefasFiltradas);
+
+    setIsOpen(false);
+  }
+
+  //zero | null | undefined
+
   return (
     <div className="container_app">
       <div className="container_form">
-        <form onSubmit={(event) => salvarTarefa(event)}>
-          <h2>Nova tarefa</h2>
+        <form
+          onSubmit={(event) => {
+            if (indexSelecionado === null) {
+              salvarTarefa(event);
+            } else {
+              editarTarefa(event);
+            }
+          }}
+        >
+          <h2>Nova tarefa - {String(indexSelecionado)}</h2>
 
           <input
             placeholder="Título"
@@ -111,9 +189,20 @@ export default function App() {
             index={index}
             tarefas={tarefas}
             setTarefas={setTarefas}
+            preencheForm={() => preencheForm(tarefa, index)}
+            setIsOpen={setIsOpen}
+            setIndexParaRemover={setIndexParaRemover}
           />
         ))}
       </div>
+
+      <Modal isOpen={modalIsOpen} style={customStyles}>
+        <h4>Confirmação</h4>
+        <p>Deseja realmente apagar o registro?</p>
+
+        <button onClick={removerTarefa}>SiM</button>
+        <button onClick={() => setIsOpen(false)}>NÃO</button>
+      </Modal>
     </div>
   );
 }
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,20 +1,21 @@
+import { useState } from "react";
 import "./styles.css";
 
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 
 export default function Card(props) {
-  const { tarefa, index, tarefas, setTarefas } = props;
+  const {
+    tarefa,
+    index,
+    tarefas,
+    setTarefas,
+    preencheForm,
+    setIsOpen,
+    setIndexParaRemover,
+  } = props;
 
-  function removerTarefa() {
-    const tarefasFiltradas = tarefas.filter(
-      (tarefa, indexTarefa) => indexTarefa !== index
-    );
-
-    localStorage.setItem("@tarefas", JSON.stringify(tarefasFiltradas));
-
-    setTarefas(tarefasFiltradas);
-  }
+  const [isEditing, setIsEditing] = useState(false);
 
   function formatarData(data) {
     const newData = new Date(data);
@@ -23,23 +24,55 @@ export default function Card(props) {
     return dataFormatada;
   }
 
+  const [titulo, setTitulo] = useState(tarefa.titulo);
+
+  function editarTarefa() {
+    const temp = [...tarefas];
+    temp[index].titulo = titulo;
+
+    const tarefasString = JSON.stringify([...temp]);
+
+    localStorage.setItem("@tarefas", tarefasString);
+
+    setTarefas(temp);
+  }
+
   return (
     <div className="card_button">
       <div className="left">
-        <p>{tarefa.titulo}</p>
+        {isEditing ? (
+          <input
+            onBlur={() => {
+              setIsEditing(false);
+              editarTarefa();
+            }}
+            value={titulo}
+            onChange={(event) => setTitulo(event.target.value)}
+          />
+        ) : (
+          <p onClick={() => setIsEditing(true)}>{tarefa.titulo}</p>
+        )}
+
         <p>
           {tarefa?.categoria ? tarefa.categoria : "Categoria nao informada"}
         </p>
+
         <p>{tarefa.descricao}</p>
       </div>
 
       <div className="right">
         <p className="date"> {formatarData(tarefa.data)}</p>
         <div>
-          <button className="btn_icon" onClick={() => alert("Editar")}>
+          <button className="btn_icon" onClick={preencheForm}>
             <FaEdit color="#0FBA3F" size={20} />
           </button>
-          <button className="btn_icon" onClick={removerTarefa}>
+          <button
+            className="btn_icon"
+            onClick={() => {
+              setIsOpen(true);
+              setIndexParaRemover(index);
+            }}
+          >
             <MdDelete color="#F90000" size={20} />
           </button>
         </div>
